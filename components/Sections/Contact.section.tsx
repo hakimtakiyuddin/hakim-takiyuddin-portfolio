d2f010df4a92a3f22c1776745884b5e8af3bbe67
Hakim Takiyuddin <hakim.takiyuddin@setel.com>
2022-05-30 20:19:08 +0800

    :bug: fix: fix unused import

--- components/Sections/Contact.section.tsx
+++ components/Sections/Contact.section.tsx
@@ -1,11 +1,6 @@
 import type { NextComponentType } from "next";
 
-import {
-  BsSpotify,
-  MdEmail,
-  AiOutlineGithub,
-  AiOutlineTwitter,
-} from "../Misc/Icons.collection";
+import { BsSpotify, MdEmail, AiOutlineGithub } from "../Misc/Icons.collection";
 
 import { Icon } from "../Misc/Icon.component";
 import { AiOutlineLinkedin } from "react-icons/ai";
@@ -13,10 +8,13 @@ import { AiOutlineLinkedin } from "react-icons/ai";
 const Contact: NextComponentType = () => {
   return (
     <div className="px-3 font-sen" id="contact">
-      <p className="text-3xl font-bold text-center text-white">Contact 👥</p>
+      <p className="text-center text-3xl font-bold text-white">Contact 👥</p>
 
       <div className="my-8 flex flex-row justify-center gap-x-4">
-        <Icon icon={<AiOutlineGithub />} url="https://github.com/hakimtakiyuddin" />
+        <Icon
+          icon={<AiOutlineGithub />}
+          url="https://github.com/hakimtakiyuddin"
+        />
 
         <Icon
           icon={<AiOutlineLinkedin />}
