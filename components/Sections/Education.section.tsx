102c4b96792c7f609a7aa76515d6a86ace8fdf9e
Hakim Takiyuddin <hakim.takiyuddin@setel.com>
2022-05-30 20:40:20 +0800

    :bug: fix: add spacing

--- components/Sections/Education.section.tsx
+++ components/Sections/Education.section.tsx
@@ -18,70 +18,72 @@ const Education: NextComponentType = () => {
         Education 🎓
       </h1>
 
-      <Grid container spacing={2} className="my-10 px-3 ">
-        <Grid item xs={6}>
-          <motion.div whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }}>
-            <div className="experience-card relative z-10 mx-4 rounded-md border bg-white p-4 shadow-xl dark:bg-gray-800">
-              <h1 className="dark:text-grey-800 md:-center-10 absolute -top-10 text-4xl font-bold text-gray-500 md:-top-10">
-                2021 - 2018
-              </h1>
-              <h1 className="text-sm font-semibold">
-                Bachelor of Software Engineering (Information System
-                Development)
-              </h1>
-              <a href={"#"} className="text-sm text-gray-500">
-                National University Of Malaysia (UKM)
-              </a>
-              <p className="text-gray mt-4 text-sm text-slate-300">
-                <span className="text-bold text-white">CGPA:</span>&nbsp; 3.76
-              </p>
-              <p className="text-gray text-sm text-slate-300 ">
-                <span className="text-white">Core Subjects:</span>&nbsp;
-                Software Requirement Engineering, Web Programming, Software
-                Testing
-              </p>
-              <p className="text-gray text-sm text-slate-300 ">
-                <span className="text-white">Awards:</span>&nbsp; Dean’s List,
-                Team Selangor Ambassador, Golden Key Honor Society’s Member,
-                Faculty Graduate Award 2020
-              </p>
-            </div>
-          </motion.div>
-        </Grid>
+      <div className="text-md my-4">
+        <Grid container spacing={2} className="my-10 px-3 ">
+          <Grid item xs={6}>
+            <motion.div whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }}>
+              <div className="experience-card relative z-10 mx-4 rounded-md border bg-white p-4 shadow-xl dark:bg-gray-800">
+                <h1 className="dark:text-grey-800 md:-center-10 absolute -top-10 text-4xl font-bold text-gray-500 md:-top-10">
+                  2021 - 2018
+                </h1>
+                <h1 className="text-sm font-semibold">
+                  Bachelor of Software Engineering (Information System
+                  Development)
+                </h1>
+                <a href={"#"} className="text-sm text-gray-500">
+                  National University Of Malaysia (UKM)
+                </a>
+                <p className="text-gray mt-4 text-sm text-slate-300">
+                  <span className="text-bold text-white">CGPA:</span>&nbsp; 3.76
+                </p>
+                <p className="text-gray text-sm text-slate-300 ">
+                  <span className="text-white">Core Subjects:</span>&nbsp;
+                  Software Requirement Engineering, Web Programming, Software
+                  Testing
+                </p>
+                <p className="text-gray text-sm text-slate-300 ">
+                  <span className="text-white">Awards:</span>&nbsp; Dean’s List,
+                  Team Selangor Ambassador, Golden Key Honor Society’s Member,
+                  Faculty Graduate Award 2020
+                </p>
+              </div>
+            </motion.div>
+          </Grid>
 
-        <Grid item xs={6}>
-          <motion.div whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }}>
-            <div className="experience-card relative z-10 mx-4 rounded-md border bg-white p-4 shadow-xl dark:bg-gray-800">
-              <h1 className="dark:text-grey-800 md:-center-10 absolute -top-10 text-4xl font-bold text-gray-500 md:-top-10">
-                2018 - 2015
-              </h1>
-              <h1 className="text-sm font-semibold">
-                Diploma in Computer Engineering
-              </h1>
-              <a href={"#"} className="text-sm text-gray-500">
-                Malaysia University of Perlis (UniMAP)
-              </a>
-              <p className="text-gray mt-4 text-sm text-slate-300">
-                <span className="text-bold text-white">CGPA:</span>&nbsp; 3.75
-              </p>
-              <p className="text-gray text-sm text-slate-300 ">
-                <span className="text-white">Core Subjects:</span>&nbsp; Digital
-                System (I & II), Microcontroller (I & II), Database System,
-                Object Oriented Programming, Electronic Circuits, Electronic
-                Instrumentations
-              </p>
-              <p className="text-gray text-sm text-slate-300 ">
-                <span className="text-white">Awards:</span>&nbsp; Dean’s List (4
-                Semesters), Anugerah Buku Konvokesyen
-              </p>
-              <motion.div
-                whileHover={{ scale: 1.2 }}
-                whileTap={{ scale: 0.8 }}
-              />
-            </div>
-          </motion.div>
+          <Grid item xs={6}>
+            <motion.div whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }}>
+              <div className="experience-card relative z-10 mx-4 rounded-md border bg-white p-4 shadow-xl dark:bg-gray-800">
+                <h1 className="dark:text-grey-800 md:-center-10 absolute -top-10 text-4xl font-bold text-gray-500 md:-top-10">
+                  2018 - 2015
+                </h1>
+                <h1 className="text-sm font-semibold">
+                  Diploma in Computer Engineering
+                </h1>
+                <a href={"#"} className="text-sm text-gray-500">
+                  Malaysia University of Perlis (UniMAP)
+                </a>
+                <p className="text-gray mt-4 text-sm text-slate-300">
+                  <span className="text-bold text-white">CGPA:</span>&nbsp; 3.75
+                </p>
+                <p className="text-gray text-sm text-slate-300 ">
+                  <span className="text-white">Core Subjects:</span>&nbsp;
+                  Digital System (I & II), Microcontroller (I & II), Database
+                  System, Object Oriented Programming, Electronic Circuits,
+                  Electronic Instrumentations
+                </p>
+                <p className="text-gray text-sm text-slate-300 ">
+                  <span className="text-white">Awards:</span>&nbsp; Dean’s List
+                  (4 Semesters), Anugerah Buku Konvokesyen
+                </p>
+                <motion.div
+                  whileHover={{ scale: 1.2 }}
+                  whileTap={{ scale: 0.8 }}
+                />
+              </div>
+            </motion.div>
+          </Grid>
         </Grid>
-      </Grid>
+      </div>
     </div>
   );
 };
